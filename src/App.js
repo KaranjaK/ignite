7d5bee599c308556305fc6d1a46f0191bf4164dc
KK <karanjakelvin77@gmail.com>
2022-06-23 19:29:18 +0300

    It will clean up the installations files and add the apps pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import Calendar from './components/Calendar';
+// import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>This is the header</h1>
       </header>
-      <Calendar/>
+      {/* <Calendar/> */}
     </div>
   );
 }
