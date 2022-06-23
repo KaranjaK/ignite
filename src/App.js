b7d569660eea6a5d56e7677779d4a5bf290f1e9d
KK <karanjakelvin77@gmail.com>
2022-06-23 20:50:57 +0300

    It will add necessary jsx and sass files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Calendar from './components/Calendar';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header>
-      </header>
+      <Home/>
       {/* <Calendar/> */}
     </div>
   );
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import './home.scss'
+
+const Home = () => {
+  return (
+    <div class='home'>Home</div>
+  )
+}
+
+export default Home
