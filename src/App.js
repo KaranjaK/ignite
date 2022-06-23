9eee61a3175222f86d1a872c49eecc04e513fb6f
KK <karanjakelvin77@gmail.com>
2022-06-23 21:39:10 +0300

    It will add paths to the various pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-// import Calendar from './components/Calendar';
+import {BrowserRouter, Routes, Route,} from 'react-router-dom'
 import Home from "./pages/home/Home";
+import Inbox from "./pages/inbox/Inbox"
+import Schedule from "./pages/schedule/Schedule"
+import Communities from "./pages/communities/Communities"
+import Analytics from "./pages/analytics/Analytics"
+import Resources from "./pages/resources/Resources"
+import Settings from "./pages/settings/Settings"
+import Login from "./pages/login/Login"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* <Calendar/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='inbox' element={<Inbox />} />
+          <Route path='schedule' element={<Schedule />} />
+          <Route path='communities' element={<Communities />} />
+          <Route path='analytics' element={<Analytics />} />
+          <Route path='resources' element={<Resources />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
