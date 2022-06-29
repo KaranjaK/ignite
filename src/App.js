e8b57e5e6664c32cf557fff9210334fd6b387eaa
KK <karanjakelvin77@gmail.com>
2022-06-29 16:52:09 +0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import {HashRouter, Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Inbox from "./pages/inbox/Inbox"
-import Schedule from "./pages/schedule/Schedule"
-import Communities from "./pages/communities/Communities"
-import Analytics from "./pages/analytics/Analytics"
-import Resources from "./pages/resources/Resources"
-import Settings from "./pages/settings/Settings"
-import Login from "./pages/login/Login"
+import Inbox from "./pages/inbox/Inbox";
+import Schedule from "./pages/schedule/Schedule";
+import Communities from "./pages/communities/Communities";
+import Analytics from "./pages/analytics/Analytics";
+import Resources from "./pages/resources/Resources";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
-      <HashRouter basename='/'>
-        <div>
-          <ul>
-            <li><Link to='/'>Login</Link></li>
-            <li><Link to='/home'>Home</Link></li>
-            <li><Link to='/inbox'>Inbox</Link></li>
-          </ul>
-
-          <Route exact path='/ignite' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/inbox' element={<Inbox />} />
-          <Route path='/schedule' element={<Schedule />} />
-          <Route path='/communities' element={<Communities />} />
-          <Route path='/analytics' element={<Analytics />} />
-          <Route path='/resources' element={<Resources />} />
-          <Route path='/settings' element={<Settings />} />
-          <Route path='/login' element={<Login />} />
-        </div>
-      </HashRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home/" element={<Home />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/communities" element={<Communities />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
